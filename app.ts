7a7e98aa4cbf9b40d697b770d5e5d02e17b81c97
deetosh <deetoshkuila18@gmail.com>
2025-03-18 23:37:14 +0530

    initial setup 2

---
+++ app.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import cookie from 'cookie-parser'
+import { ENV_FE_BASE_URL, ENV_PORT } from './secret';
+import RouterConfig from './src/config/router_config';
+import BUS_TRACK_DB from './src/config/db/database';
+
+const app: express.Application = express();
+const port = ENV_PORT;
+
+app.use(cors({
+    origin: [
+        `${ENV_FE_BASE_URL}`
+    ],
+    credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookie());
+
+
+// Configure the routes
+const routerConfig = new RouterConfig(app);
+routerConfig.configureRoutes();
+
+// Connect the database
+const dbConn = BUS_TRACK_DB.getConnection();
+
+// Listen on the server
+const runningMessage = `Server running at port : ${port}`
+
+app.listen(port, () => {
+    console.log(runningMessage);
+});
---
+++ secret.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+const dotenvResult = dotenv.config({ path: `.env.${process.env.ENVIRONMENT}` });
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
+
+// exporting all the environment variables
+
+export const ENV_PORT = process.env.PORT;
+export const ENV_NODE_ENV = process.env.NODE_ENV;
+export const ENV_FE_BASE_URL = process.env.FE_BASE_URL;
+
+export const ENV_DB_NAME = process.env.DB_NAME;
+export const ENV_DB_USER = process.env.DB_USER;
+export const ENV_DB_HOST = process.env.DB_HOST;
+export const ENV_DB_PASSWORD = process.env.DB_PASSWORD;
+export const ENV_DB_PORT = process.env.DB_PORT;
+
+export const ENV_ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+export const ENV_ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+export const ENV_GRAM_EMAIL = process.env.GRAM_EMAIL;
+export const ENV_GRAM_PASSWORD = process.env.GRAM_PASSWORD;
